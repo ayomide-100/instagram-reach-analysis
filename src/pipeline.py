14525f7b2f0402bd3acbe07704bae13c2e4fa3f7
Ayomide Adegoke <ayadegoke.lfis@gmail.com>
2025-08-20 03:54:15 +0100

    updated changes to pipepline

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,42 +5,35 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer, make_column_transformer, make_column_selector
-from xgboost import XGBRegressor
-from column_functions import TfidfWrapper, ravel_values, DateTimeConverter
 
-
-flatten = FunctionTransformer(ravel_values, validate=False)
-
-vectorizer = make_pipeline(flatten, TfidfVectorizer(preprocessor=None, lowercase=False))
-
-scale = Pipeline([
- ("scaler", StandardScaler())
-
-])
+#from column_functions import TfidfWrapper, ravel_values, DateTimeConverter
 
 
 
-text_features = ['Caption', 'Hashtags']
 
-scale_features = ['CaptionLength', 'DayOfWeek', 'Month',
-'HourOfDay', 'HashtagCount']
 
-passthrough_features = ['HashtagDensity', 'IsWeekend']
 
-timestamp_feature = ['Timestamp']
 
-cat_feature = ['Content Type']
 
+scale_features = [
+    "Likes", "Comments", "Saves",
+    "Shares", "Profile Visits",
+    "CaptionLength", "HashtagCount", 
+    "DayOfWeek", "HourOfDay", "Month"
+]
+
+passthrough_features = ["IsWeekend", "HashtagDensity"]
+
+full_features = scale_features + passthrough_features
+
+target = ["Impressions"]
 
 
 
 
 preprocessor = ColumnTransformer(
     transformers=[
-('text', TfidfVectorizer(max_features=100, min_df=2, ngram_range=(1, 2)), 'Caption'), 
-('hashtags', TfidfVectorizer(max_features=100, min_df=2, ngram_range=(1, 2)), 'Hashtags'), 
- ('scale', StandardScaler(), scale_features),
- ('encode', OneHotEncoder(), cat_feature)
+        ('scale', StandardScaler(), scale_features)
 ]
 
 )
